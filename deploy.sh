d124a38319261d4c89494906e7d6b4114ed64291
alxwolf <alexander.wolf@gmx.net>
2022-10-29 12:06:28 +0200

    fix export and getcwd errors

--- deploy.sh
+++ deploy.sh
@@ -15,13 +15,12 @@ deploy_acmesh() {
 case "$(ubnt-device-info model || true)" in
 	"UniFi Dream Machine Pro"|"UniFi Dream Machine")
 	echo "UDM/UDM Pro detected, installing ubios-cert in /mnt/data..."
-	DATA_DIR="/mnt/data"
+	export DATA_DIR="/mnt/data"
 	;;
 	"UniFi Dream Router"|"UniFi Dream Machine SE")
 	echo "UDR/UDM SE detected, installing ubios-cert in /data..."
 	sed -i 's#/mnt/data#/data#g' "${SCRIPT_DIR}/ubios-cert/ubios-cert.env" "${SCRIPT_DIR}/ubios-cert/ubios-cert.sh" "${SCRIPT_DIR}/ubios-cert/on_boot.d/99-ubios-cert.sh"
-	DATA_DIR="/data"
-	export DATA_DIR
+	export DATA_DIR="/data"
 	;;
 	*)
 	echo "Unsupported model: $(ubnt-device-info model)" 1>&2
@@ -33,5 +32,6 @@ echo
 deploy_acmesh
 chmod +x ${SCRIPT_DIR}/ubios-cert/ubios-cert.sh ${SCRIPT_DIR}/ubios-cert/on_boot.d/99-ubios-cert.sh
 mv "${SCRIPT_DIR}/ubios-cert/" "${DATA_DIR}/ubios-cert/"
-rm -rf ${SCRIPT_DIR}/../ubios-cert-main ubios-cert.zip
+rm -rf ${SCRIPT_DIR}/../ubios-cert-main ~/ubios-cert.zip
 echo "Deployed with success in ${DATA_DIR}/ubios-cert"
+cd ~
