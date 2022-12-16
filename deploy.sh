877c6a5583b377770488820a7bb8a31b7c442633
Dennis Gilmore <dennis@ausil.us>
2022-12-15 22:59:16 -0600

    update logic to set DATA_DIR based on firmware version
    With the UDM and UDM-PRO getting 2.x firmware in early access there
    is a need to support different locations based on firmware version
    and not model
    
    Signed-off-by: Dennis Gilmore <dennis@ausil.us>

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 set -e
 SCRIPT_DIR=$(dirname ${0})
 
+# Set default data path
+export DATA_DIR="/mnt/data"
+
 deploy_acmesh() {
 	echo "acme.sh will be deployed inside ubios-cert to persist firmware updates"
 	ACME_URL=$(curl -s https://api.github.com/repos/acmesh-official/acme.sh/releases/latest | grep tarball_url | awk '{ print $2 }' | sed 's/,$//' | sed 's/"//g')
@@ -12,15 +15,16 @@ deploy_acmesh() {
 	tar -xvf acmesh.tar.gz --directory="${SCRIPT_DIR}/ubios-cert/acme.sh" --strip-components=1 
 }
 
-case "$(ubnt-device-info model || true)" in
-	"UniFi Dream Machine Pro"|"UniFi Dream Machine")
-	echo "UDM/UDM Pro detected, installing ubios-cert in /mnt/data..."
-	export DATA_DIR="/mnt/data"
-	;;
-	"UniFi Dream Router"|"UniFi Dream Machine SE")
-	echo "UDR/UDM SE detected, installing ubios-cert in /data..."
-	sed -i 's#/mnt/data#/data#g' "${SCRIPT_DIR}/ubios-cert/ubios-cert.env" "${SCRIPT_DIR}/ubios-cert/ubios-cert.sh" "${SCRIPT_DIR}/ubios-cert/on_boot.d/99-ubios-cert.sh"
-	export DATA_DIR="/data"
+if [ $(ubnt-device-info firmware || true) > 2 ]
+ then
+        sed -i 's#/mnt/data#/data#g' "${SCRIPT_DIR}/ubios-cert/ubios-cert.env" "${SCRIPT_DIR}/ubios-cert/ubios-cert.sh" "${SCRIPT_DIR}/ubios-cert/on_boot.d/99-ubios-cert.sh"
+        export DATA_DIR="/data"
+fi
+
+MODEL=$(ubnt-device-info model || true)
+case "${MODEL}" in
+	"UniFi Dream Machine Pro"|"UniFi Dream Machine"|"UniFi Dream Router"|"UniFi Dream Machine SE")
+	echo "${MODEL} detected, installing ubios-cert in ${DATA_DIR}..."
 	;;
 	*)
 	echo "Unsupported model: $(ubnt-device-info model)" 1>&2
