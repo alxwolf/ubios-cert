66d6727f7b6ddbe994b724e4f377d2389a0de177
alxwolf <alexander.wolf@gmx.net>
2024-10-12 20:50:57 +0200

    Trying to accommodate for all the fancy new stuff since 3.2.7 - starting with the UniFI Core application

--- ubios-cert/ubios-cert.sh
+++ ubios-cert/ubios-cert.sh
@@ -3,7 +3,28 @@
 #
 # based on the fine work of kchristensen/udm-le
 # https://github.com/kchristensen/udm-le
+# and
+# Glenn Rietveld https://GlennR.nl
 #
+
+# Foreground colors
+#  0;30   Black          1;30   Dark Gray
+#  0;34   Blue           1;34   Light Blue
+#  0;32   Green          1;32   Light Green
+#  0;36   Cyan           1;36   Light Cyan
+#  0;31   Red            1;31   Light Red
+#  0;35   Purple         1;35   Light Purple
+#  0;33   Brown          1;33   Yellow
+#  0;37   Light Gray     1;37   White
+
+RESET='\e[0m'
+YELLOW='\e[1;33m'
+GRAY='\e[0;37m'
+BLUE='\e[0;34m'
+RED='\e[1;31m'
+GREEN='\e[1;32m'
+
+unifi_core_device=$(grep -io "welcome.*" /etc/motd | sed -e 's/Welcome //g' -e 's/to //g' -e 's/the //g' -e 's/!//g');
 
 set -e
 
@@ -19,24 +40,48 @@ NEW_CERT='no'
 
 # identify device firmware version: <2 is legacy (podman), 2+ is current (baremetal)
 
-IS_UNIFI_2='false'
+IS_UNIFI_4='false'
+FIRMWARE_VER=$(ubnt-device-info firmware)
 if [ $(ubnt-device-info firmware | sed 's#\..*$##g' || true) -gt 1 ]
  then
-	IS_UNIFI_2='true'
+	IS_UNIFI_4='true'
+	echo -e "${GREEN}SUCCESS${RESET} Supported firmware: ${FIRMWARE_VER} on ${unifi_core_device}. Moving on."
  else
-	echo "Unsupported firmware: ${FIRMWARE_VER}"
+	echo -e "${RED}FAILED ${RESET} Unsupported firmware: ${FIRMWARE_VER} on ${unifi_core_device}. Stopping."
 	exit 1
 fi
 
 deploy_cert() {
+	echo -e "${GRAY}INFO   ${RESET} New certificate was generated, time to deploy it"
 	if [ "$(find -L "${ACMESH_ROOT}" -type f -name fullchain.cer -mmin -5)" ]; then
-		echo 'New certificate was generated, time to deploy it'
-		# Controller certificate - copy the full chain certificates to unifi-core.crt to avoid Java cert store command error
-		cp -f ${ACMESH_ROOT}/${CERT_NAME}/fullchain.cer ${UNIFIOS_CERT_PATH}/unifi-core.crt
-		cp -f ${ACMESH_ROOT}/${CERT_NAME}/${CERT_NAME}.key ${UNIFIOS_CERT_PATH}/unifi-core.key
+		copy_and_link
 		NEW_CERT='yes'
+		echo -e "${GREEN}# SUCCESS${RESET} Certifcate deployed to UniFi OS, service not yet restarted."
 	else
-		echo 'No new certificate was found, exiting without restart'
+		echo -e  "${BLUE}# INFO    ${RESET} No new certificate was found."
+	fi
+}
+
+copy_and_link() {
+	if [[ ! -d ${UBIOS_CERT_ROOT}/certificates/ ]]; then
+		mkdir -p ${UBIOS_CERT_ROOT}/certificates/;
+		echo -e "${BLUE}# INFO    ${RESET} Created directory for certificates"
+	fi
+	# copy cert and key to a more convenient place so we can use fixed path names
+	if [[ -f "${ACMESH_ROOT}/${CERT_NAME}/fullchain.cer" ]]; then
+		cp -f ${ACMESH_ROOT}/${CERT_NAME}/fullchain.cer ${UBIOS_CERT_ROOT}/certificates/unifi-core.crt
+	fi
+	if [[ -f "${ACMESH_ROOT}/${CERT_NAME}/${CERT_NAME}.key" ]]; then
+		cp -f ${ACMESH_ROOT}/${CERT_NAME}/${CERT_NAME}.key ${UBIOS_CERT_ROOT}/certificates/unifi-core.crt
+	fi
+	# beginning with 3.2.7, no need to copy the cert and key, but point in the right direction via a YAML file
+	if [[ ! -f "${UNIFI_CORE_SSL_CONFIG}" ]]; then
+		tee "${UNIFI_CORE_SSL_CONFIG}" &>/dev/null << SSL
+# File created by ubios-cert (certificates for Unifi Dream Machines).
+ssl:
+  crt: '${UBIOS_CERT_ROOT}/certificates/unifi-core.crt'
+  key: '${UBIOS_CERT_ROOT}/certificates/unifi-core.key'
+SSL
 	fi
 }
 
@@ -94,8 +139,12 @@ add_radius() {
 
 unifios_restart () {
 	echo "Please wait while restarting unifi-core using 'systemctl restart unifi-core'"
-	# Restarting the network app with 'restart unifi' has no effect on cert. The whole unifi-os has to be reloaded.
-	systemctl restart unifi-core
+	if systemctl restart unifi-core; then 
+		echo -e "${GREEN}# SUCCESS${RESET} Restarted UniFi OS on ${unifi_core_device}!"
+	else 
+		echo -e "${RED}# FAILED ${RESET} Restarted UniFi OS on ${unifi_core_device}..."
+	fi
+	 sleep 2;
 }
 
 remove_old_log() {
@@ -110,20 +159,26 @@ remove_cert() {
 	echo "Executing: ${ACME_CMD} --remove ${DOMAINS}"
 	remove_old_log
 	${ACME_CMD} --remove ${DOMAINS}
-	echo "Removed certificates from acme.sh renewal. The certificate files can now manually be removed."
+	echo -e "${GREEN}SUCCESS${RESET} Removed certificates from acme.sh renewal. The certificate files can now manually be removed."
 }
+
+######################
+# 
+# "main" starts here
+#
+######################
 
 # Check for and if it not exists create acme.sh directory so the container can write to it - owner "nobody"
 if [ ! -d "${ACMESH_ROOT}" ]; then
 	mkdir "${ACMESH_ROOT}"
 	chmod 700 "${ACMESH_ROOT}"
-	echo "Created directory 'acme.sh'"
+	echo -e "${GRAY}INFO   ${RESET} Created directory 'acme.sh'"
 fi
 
 # Check for correct permissions and adjust if necessary
 if [ "$(stat -c '%u:%g' "${ACMESH_ROOT}")" != "65534:65534" ]; then
 	chown 65534:65534 "${ACMESH_ROOT}"
-	echo "Adjusted permissions for 'acme.sh'"
+	echo -e "${GRAY}INFO   ${RESET} Adjusted permissions for 'acme.sh'"
 fi
 
 # Support multiple certificate SANs
@@ -139,6 +194,13 @@ done
 ACME_HOME="--config-home ${ACMESH_ROOT} --cert-home ${ACMESH_ROOT} --home ${ACMESH_ROOT}"
 ACME_CMD="${ACMESH_ROOT}/acme.sh ${ACMESH_CMD_PARAMS} ${ACME_HOME}"
 
+# confirm if 'account.conf' exists and can only be accessed by owner (nobody / nogroup)
+if [ -f "${ACMESH_ROOT}/account.conf" ]; then
+	if [ "$(stat -c '%a' "${ACMESH_ROOT}/account.conf")" != "600" ]; then
+		chmod 600 ${ACMESH_ROOT}/account.conf
+	fi
+fi
+
 # Setup nightly cron job
 CRON_FILE='/etc/cron.d/ubios-cert'
 if [ ! -f "${CRON_FILE}" ]; then
@@ -150,36 +212,35 @@ if [ ! -f "${CRON_FILE}" ]; then
 	if [ -f /etc/init.d/cron ]; then
 		/etc/init.d/cron reload ${CRON_FILE}
 	else
-		echo "ERROR: Could not find cron service at /etc/init.d/cron" >&2
+		echo -e -e "${RED}FAILED ${RESET} Could not find cron service at /etc/init.d/cron" >&2
 		exit 1
 	fi
-	echo "Restored cron file"
-fi
-
-# confirm if 'account.conf' exists and can only be accessed by owner (nobody / nogroup)
-if [ -f "${ACMESH_ROOT}/account.conf" ]; then
-	if [ "$(stat -c '%a' "${ACMESH_ROOT}/account.conf")" != "600" ]; then
-		chmod 600 ${ACMESH_ROOT}/account.conf
-	fi
+	echo -e "${GREEN}SUCCESS${RESET} Restored cron file"
 fi
 
 case $1 in
 initial)
 	remove_old_log
-	echo "Setting default CA to ${DEFAULT_CA}"
+	echo "${GRAY}INFO   ${RESET} Setting default CA to ${DEFAULT_CA}"
 	${ACME_CMD} --set-default-ca --server ${DEFAULT_CA}
-	echo "Attempting initial certificate generation"
+	echo "${GRAY}INFO   ${RESET} Attempting initial certificate generation"
 	${ACME_CMD} --register-account --email ${CA_REGISTRATION_EMAIL}
-	${ACME_CMD} --issue ${DOMAINS} --dns ${DNS_API_PROVIDER} --keylength 2048 ${LOG} && deploy_cert && add_captive && add_radius && unifios_restart
+	${ACME_CMD} --issue ${DOMAINS} --dns ${DNS_API_PROVIDER} --keylength 2048 ${LOG}
+	deploy_cert
+	add_captive
+	add_radius
+	unifios_restart
 	;;
 renew)
 	remove_old_log
-	echo "Attempting acme.sh upgrade"
+	echo -e "${GRAY}INFO   ${RESET} Attempting acme.sh upgrade"
 	${ACME_CMD} --upgrade
-	echo "Attempting certificate renewal"
+	echo -e "${GRAY}INFO   ${RESET} Attempting certificate renewal"
 	${ACME_CMD} --renew ${DOMAINS} --dns ${DNS_API_PROVIDER} --keylength 2048 ${LOG} && deploy_cert
 	if [ "${NEW_CERT}" = "yes" ]; then
-		add_captive && add_radius && unifios_restart
+		add_captive
+		add_radius
+		nifios_restart
 	fi
 	;;
 forcerenew)
@@ -187,15 +248,35 @@ forcerenew)
 	remove_old_log
 	${ACME_CMD} --renew ${DOMAINS} --force --dns ${DNS_API_PROVIDER} --keylength 2048 ${LOG} && deploy_cert
 	if [ "${NEW_CERT}" = "yes" ]; then
-		add_captive && add_radius && unifios_restart
+		add_captive
+		add_radius
+		unifios_restart
 	fi
 	;;
 deploy)
 	echo "Deploying certificates and restarting UniFi OS"
-	deploy_cert && 	add_captive && add_radius && unifios_restart
+	deploy_cert
+	add_captive
+	add_radius
+	unifios_restart
+	;;
+deploy-core)
+	echo -e "${GRAY}INFO   ${RESET} Deploying certificate to unifi-core and restarting UniFi OS"
+	deploy_cert
+	unifios_restart
+	;;
+deploy-networkapp)
+	echo -e "${GRAY}INFO   ${RESET} Deploying certificate to Network Application and restarting UniFi OS"
+	add_captive
+	unifios_restart
+	;;
+deploy-radius)
+	echo -e "${GRAY}INFO   ${RESET} Deploying certificate to RADIUS server and restarting UniFi OS"
+	add_radius
+	unifios_restart
 	;;
 setdefaultca)
-	echo "Setting default CA to ${DEFAULT_CA}"
+	echo -e "${GRAY}INFO   ${RESET}Setting default CA to ${DEFAULT_CA}"
 	remove_old_log
 	${ACME_CMD} --set-default-ca --server ${DEFAULT_CA}
 	;;
@@ -208,9 +289,9 @@ cleanup)
 	if [ -f "${ACMESH_ROOT}/account.conf" ]; then
 		remove_old_log
 		remove_cert
-		echo "Executing: ${ACME_CMD} --deactivate-account"
+		echo -e "${GRAY}INFO   ${RESET} Executing: ${ACME_CMD} --deactivate-account"
 		${ACME_CMD} --deactivate-account
-		echo "Deactivated LE account"
+		echo -e "${GRAY}INFO   ${RESET} Deactivated LE account"
 	fi
 	;;
 esac
